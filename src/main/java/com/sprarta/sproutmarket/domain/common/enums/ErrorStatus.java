c999f83f9a850c58610be4d2d0a8275e1335dcfc
이지택 <mkojibhu@naver.com>
2024-10-23 17:46:10 +0900

    merge

--- src/main/java/com/sprarta/sproutmarket/domain/common/enums/ErrorStatus.java
+++ src/main/java/com/sprarta/sproutmarket/domain/common/enums/ErrorStatus.java
@@ -22,7 +22,7 @@ public enum ErrorStatus implements BaseCode {
     TEST_ERROR(HttpStatus.BAD_REQUEST, 400, "ApiException 예외 처리 테스트"),
 
     // user 예외처리
-    NOT_FOUND_EMAIL(HttpStatus.NOT_FOUND, 404, "이미 존재하는 이메일입니다."),
+    BAD_REQUEST_EMAIL(HttpStatus.NOT_FOUND, 404, "이미 존재하는 이메일입니다."),
     NOT_FOUND_AUTH_USER(HttpStatus.NOT_FOUND, 404, "가입되지 않은 유저입니다."),
     BAD_REQUEST_USER(HttpStatus.BAD_REQUEST, 404, "비활성화된 계정입니다. 관리자에 문의하세요."),
     NOT_FOUND_USER(HttpStatus.NOT_FOUND, 404, "존재하지 않는 사용자입니다."),
